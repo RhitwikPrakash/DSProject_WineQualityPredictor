ef58178279e13979f3e12f16f0866fcb4343614b
Rhitwik Prakash <rhitwik.2005@example.com>
2025-07-13 01:55:57 +0530

    Committing again

---
+++ src/Data_science_project/config/configuration.py
@@ -0,0 +1,10 @@
+# src/Data_science_project/config/configuration.py
+
+class ConfigurationManager:
+    def __init__(self, config_filepath, params_filepath, schema_filepath):
+        # your logic here
+        pass
+
+    def get_data_ingestion_config(self):
+        # return some config
+        pass
